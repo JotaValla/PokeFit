d93a2749b8824cbdabf57d48927a1b5cd0d89f9c
Fsmicos <michael.trocellier@epn.edu.ec>
2025-06-12 22:13:58 -0500

    feat: creacion de las pantallas de encuestas, objetivos y eleccion de pokemon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,15 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    
+    // Biblioteca para GIFs
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.25")
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
